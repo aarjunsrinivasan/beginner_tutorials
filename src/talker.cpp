66afe21714a8fa9c68ffb679d770de18965ed930
aarjunsrinivasan <arjunsrinivasan.a@gmail.com>
2020-10-24 14:44:08 -0400

    [Talker] publish custom msg

--- src/talker.cpp
+++ src/talker.cpp
@@ -63,7 +63,7 @@ int main(int argc, char **argv)
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "hello world " << count;
+    ss << "Robots are awesome " << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
